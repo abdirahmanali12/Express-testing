6f0f04f474c989659752e5ef6a2a4d846cb11fd2
maancode <abdirahmanaliyousuf@gmail.com>
2021-11-14 17:19:56 +0300

    first start

---
+++ server-2/index.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const port = 3000;
+const app = express();
+app.get("/", (req, res) => {
+  res.send("<h4>Server on port: " + port + "</h4>");
+});
+app.listen(port, () => console.log("started ", port));
